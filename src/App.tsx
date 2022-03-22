85cff4a59066865444e1e9f20e47bb2d82887339
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-03-22 17:09:49 -0300

    adicionado o array pra armazenamento das listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import * as C from './app.styles'
+import {useState} from 'react';
+import * as C from './app.styles';
+import {Item} from './types/Files';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'estudar', done: false},
+    {id: 2, name: 'trabalhar', done: false},
+  ]);
     return (
       <C.Container>
         <C.Area>
-          ...
+          <C.Header>Lista de Tarefas</C.Header>
+
+            {/* area de novas tarefas */}
+
+            {list.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+
         </C.Area>
       </C.Container>
     );
