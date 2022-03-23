173931e23ce601bbcf5aa40754c0ea0235fc6928
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-03-23 17:27:23 -0300

    Fim do projeto base.

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,29 @@ import { AddArea } from './components/AddArea';
 const App = () => {
   const [list, setList] = useState<Item[]>([
     {id: 1, name: 'Estudar', done: false},
-    {id: 2, name: 'Trabalhar', done: true},
-    {id: 3, name: 'Lavar Vasilhas', done: false},
-    {id: 4, name: 'Treinar mais', done: true},
+    {id: 2, name: 'Trabalhar', done: false},
+    {id: 3, name: 'Caminhar', done: false},
+    {id: 4, name: 'Treinar', done: false},
+
   ]);
+
+    const handleAddTask = (taskName: string) => {
+      let newList = [...list];
+      newList.push({
+        id: list.length + 1,
+        name: taskName, 
+        done: false
+      });
+      setList(newList);
+    }
+
+
     return (
       <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
 
-            <AddArea />
+            <AddArea onEnter= {handleAddTask} />
 
             {list.map((item, index) => (
               <ListItem key={index} item={item} />
